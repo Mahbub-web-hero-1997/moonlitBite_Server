57af9a6d525c4bb02a3349168e4996f9f2e5ddca
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-12-16 13:53:48 +0600

    all users get api added

--- index.js
+++ index.js
@@ -114,8 +114,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result= await cartCollection.deleteOne(query)
-      res.send(result)
-      
+      res.send(result)      
     })
 
     // User Api
