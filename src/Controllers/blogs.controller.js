54eaab9ac4116815ca6a62b09b1503e73c96ece4
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-03-19 15:10:23 +0600

    get single blog by id api added

--- src/Controllers/blogs.controller.js
+++ src/Controllers/blogs.controller.js
@@ -4,41 +4,53 @@ import ApiResponse from "../Utils/ApiResponse.js";
 import asyncHandler from "../Utils/AsyncHandler.js";
 import uploadOnCloudinary from "../Utils/Cloudinary.js";
 // Create A blog
-const createBlog=asyncHandler(async(req, res)=>{
-    const { title, content } = req.body;
-    if(!title || !content){
-        throw new ApiErrors(400,"All fields are required")
-    }
-    if([title, content].some(field=>field.trim()=="")){
-        throw new ApiErrors(400,"All fields should not be empty")
-    }
-const imageLocalPath =req.files.map((file) => file.path);
+const createBlog = asyncHandler(async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    throw new ApiErrors(400, "All fields are required");
+  }
+  if ([title, content].some((field) => field.trim() == "")) {
+    throw new ApiErrors(400, "All fields should not be empty");
+  }
+  const imageLocalPath = req.files.map((file) => file.path);
 
-const imageUploadPromises = imageLocalPath.map((path) =>
-  uploadOnCloudinary(path)
-);
-const imageResults = await Promise.all(imageUploadPromises);
-const uploadImages = imageResults
-  .filter((result) => result)
-  .map((result) => result.url);
+  const imageUploadPromises = imageLocalPath.map((path) =>
+    uploadOnCloudinary(path)
+  );
+  const imageResults = await Promise.all(imageUploadPromises);
+  const uploadImages = imageResults
+    .filter((result) => result)
+    .map((result) => result.url);
   console.log(uploadImages);
-if (imageResults.length === 0) {
-  throw new ApiErrors("Failed to upload images", 500);
-}
-
-const blog = await Blogs.create({
-  title,
-  content,
-  images: uploadImages,
-});
-if (imageResults.length === 0) {
+  if (imageResults.length === 0) {
     throw new ApiErrors("Failed to upload images", 500);
   }
-res.status(200).json(new ApiResponse(201,blog, "Blog successfully uploaded"))
-})
+
+  const blog = await Blogs.create({
+    title,
+    content,
+    images: uploadImages,
+  });
+  if (imageResults.length === 0) {
+    throw new ApiErrors("Failed to upload images", 500);
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(201, blog, "Blog successfully uploaded"));
+});
 // get all blogs
-const getAllBlogs=asyncHandler(async(req,res)=>{
-    const blogs = await Blogs.find({});
-    res.status(200).json(new ApiResponse(200,blogs, "All blogs fetched successfully"))
-})
-export { createBlog, getAllBlogs };
+const getAllBlogs = asyncHandler(async (req, res) => {
+  const blogs = await Blogs.find({});
+  res
+    .status(200)
+    .json(new ApiResponse(200, blogs, "All blogs fetched successfully"));
+});
+// get single blog by id
+const getSingleBlog = asyncHandler(async (req, res) => {
+  const blog = await Blogs.findById(req.params.id);
+  if (!blog) {
+    throw new ApiErrors(404, "Blog not found");
+  }
+  res.status(200).json(new ApiResponse(200, blog, "Blog fetched successfully"));
+});
+export { createBlog, getAllBlogs, getSingleBlog };
--- src/Routes/blogs.route.js
+++ src/Routes/blogs.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createBlog, getAllBlogs } from "../Controllers/blogs.controller.js";
+import { createBlog, getAllBlogs, getSingleBlog } from "../Controllers/blogs.controller.js";
 import upload from "../Middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/create").post(upload.array("images",4), createBlog);
 router.route("/all").get(getAllBlogs)
+router.route("/single/:id").get(getSingleBlog);
 
 export default router;
