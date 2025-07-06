ddad4ea5807772ec6cbb790574518b6b5a0e2f7f
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-07-06 11:49:51 +0600

    party model created

---
+++ src/Models/party.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+const partySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["available", "unavailable"],
+      default: "available",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Party = mongoose.model("Party", partySchema);
+export default Party;
