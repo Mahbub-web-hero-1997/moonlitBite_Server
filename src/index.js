f8d25c98c563f5564525c7d86797bf6aa3184bbc
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-07-07 16:30:34 +0600

    a error fixed on ConnectionDB.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import dotenv from "dotenv"
-import app from "./app.js"
-import connectionDB from "./DB/ConnectionDb.js"
+import dotenv from "dotenv";
+import app from "./app.js";
+import ConnectionDB from "./DB/ConnectionDb.js";
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT;
 
-connectionDB()
-.then(()=>{
-    app.listen(port,() => {
+ConnectionDB()
+  .then(() => {
+    app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
-})
-  .catch(err => console.error("Error connecting to the database", err));
+  })
+  .catch((err) => console.error("Error connecting to the database", err));
