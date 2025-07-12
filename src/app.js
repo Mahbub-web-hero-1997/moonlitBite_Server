6413b7ef2214d7c0aab64a01c4e1daf477528f41
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-07-12 11:40:24 +0600

    some changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173, https://moonlit-bites.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true,
     // methods: ["GET", "POST", "PUT", "DELETE"],
   })
