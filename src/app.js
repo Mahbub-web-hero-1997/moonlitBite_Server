641ddf45113744b4ca240411e04aac706b98ff3b
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-07-14 15:47:27 +0600

    try to fixed error

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://moonlitbite-server.onrender.com/",
+    ],
     credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(express.static("./public/"));
